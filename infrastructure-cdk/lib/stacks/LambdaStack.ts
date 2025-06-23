d7728f4753f6c57d49d7a44812788b17ab209cb2
Sandjio <emmasandjio@gmail.com>
2025-06-23 16:53:03 +0100

    feat: enhance LambdaStack with additional handlers and EventBridge integration

--- infrastructure-cdk/lib/stacks/LambdaStack.ts
+++ infrastructure-cdk/lib/stacks/LambdaStack.ts
@@ -5,6 +5,8 @@ import {
   aws_lambda_nodejs as lambdaNodejs,
   aws_dynamodb as dynamodb,
   aws_lambda as lambda,
+  aws_events as events,
+  aws_events_targets as targets,
   aws_iam as iam,
   aws_logs as logs,
   Duration,
@@ -15,13 +17,20 @@ import * as path from "path";
 
 interface LambdaStackProps extends StackProps {
   table: dynamodb.ITable;
+  eventBus: events.IEventBus;
 }
 
 export class LambdaStack extends Stack {
   public readonly webhookHandler: lambdaNodejs.NodejsFunction;
+  public readonly recordGithubEventDetailsHandler: lambdaNodejs.NodejsFunction;
+  public readonly fetchDiffedChangesHandler: lambdaNodejs.NodejsFunction;
+  public readonly lambdaAnalyzeDiff: lambdaNodejs.NodejsFunction;
 
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
+
+    // Define stageName, e.g., from stack name, context, or environment
+    const stageName = this.node.tryGetContext("stage") || "dev";
 
     this.webhookHandler = new lambdaNodejs.NodejsFunction(
       this,
@@ -29,22 +38,21 @@ export class LambdaStack extends Stack {
       {
         entry: path.join(__dirname, "..", "..", "lambda", "index.ts"),
         runtime: lambda.Runtime.NODEJS_22_X,
-        handler: "index.handler",
         bundling: {
           externalModules: ["aws-lambda", "@aws-sdk/client-secrets-manager"],
         },
         projectRoot: path.join(__dirname, "../.."),
         environment: {
-          TABLE_NAME: props.table.tableName,
+          EVENT_BUS_NAME: props.eventBus.eventBusName,
         },
         timeout: Duration.seconds(10),
         memorySize: 256,
         logRetention: logs.RetentionDays.ONE_WEEK,
+        insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0,
       }
     );
 
-    props.table.grantWriteData(this.webhookHandler);
-
+    // Grant permissions to get the secret value from Secrets Manager
     this.webhookHandler.addToRolePolicy(
       new iam.PolicyStatement({
         actions: ["secretsmanager:GetSecretValue"],
@@ -53,6 +61,129 @@ export class LambdaStack extends Stack {
         ],
       })
     );
+    // Grant permissions to the webhook handler to send events to the EventBridge bus
+    props.eventBus.grantPutEventsTo(this.webhookHandler);
+
+    this.recordGithubEventDetailsHandler = new lambdaNodejs.NodejsFunction(
+      this,
+      "RecordGithubEventDetailsHandler",
+      {
+        entry: path.join(
+          __dirname,
+          "..",
+          "..",
+          "lambda",
+          "recordGithubEventDetails.ts"
+        ),
+        runtime: lambda.Runtime.NODEJS_22_X,
+        environment: { TABLE_NAME: props.table.tableName },
+        bundling: {
+          externalModules: ["aws-lambda", "@aws-sdk/client-secrets-manager"],
+        },
+        projectRoot: path.join(__dirname, "../.."),
+        timeout: Duration.seconds(10),
+        memorySize: 256,
+        logRetention: logs.RetentionDays.ONE_WEEK,
+      }
+    );
+    // Grant permissions to the recordGithubEventDetailsHandler to write to the DynamoDB table
+    props.table.grantWriteData(this.recordGithubEventDetailsHandler);
+
+    this.fetchDiffedChangesHandler = new lambdaNodejs.NodejsFunction(
+      this,
+      "FetchDiffedChangesHandler",
+      {
+        entry: path.join(
+          __dirname,
+          "..",
+          "..",
+          "lambda",
+          "fetchDiffedChanges.ts"
+        ),
+        runtime: lambda.Runtime.NODEJS_22_X,
+        environment: { EVENT_BUS_NAME: props.eventBus.eventBusName },
+        bundling: {
+          externalModules: ["aws-lambda", "@aws-sdk/client-secrets-manager"],
+        },
+        projectRoot: path.join(__dirname, "../.."),
+        timeout: Duration.seconds(10),
+        memorySize: 256,
+        logRetention: logs.RetentionDays.ONE_WEEK,
+      }
+    );
+    // Grant permissions to the fetchDiffedChangesHandler  to send events to the EventBridge bus
+    props.eventBus.grantPutEventsTo(this.fetchDiffedChangesHandler);
+
+    this.lambdaAnalyzeDiff = new lambdaNodejs.NodejsFunction(
+      this,
+      "LambdaAnalyzeDiff",
+      {
+        entry: path.join(__dirname, "..", "..", "lambda", "analyzeDiff.ts"),
+        runtime: lambda.Runtime.NODEJS_22_X,
+        environment: {
+          EVENT_BUS_NAME: props.eventBus.eventBusName,
+          MODEL_ID: "anthropic.claude-3-sonnet-20240229", // Store the value in SSM parameter store or Secrets Manager
+        },
+        bundling: {
+          externalModules: ["aws-lambda", "@aws-sdk/client-secrets-manager"],
+        },
+        projectRoot: path.join(__dirname, "../.."),
+        timeout: Duration.seconds(10),
+        memorySize: 256,
+        logRetention: logs.RetentionDays.ONE_WEEK,
+      }
+    );
+    // Grant permissions to the lambdaAnalyzeDiff to send prompts to Bedrock
+    // TODO: Check the correctness of the policy statement below
+    this.lambdaAnalyzeDiff.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          "bedrock:InvokeModel",
+          "bedrock:InvokeModelWithResponseStream",
+        ],
+        resources: [
+          `arn:${this.partition}:bedrock:${this.region}:${this.account}:foundation-model/vortex-analyze-diff`,
+        ],
+      })
+    );
+
+    new events.Rule(this, `PRDataToDynamoRule-${stageName}`, {
+      eventBus: props.eventBus,
+      enabled: true,
+      ruleName: `RecordGithubEventDetailsRule-${stageName}`,
+      description: "Records PR and commit details to DynamoDB",
+      eventPattern: {
+        source: ["vortex.github"],
+        detailType: ["pr.created", "pr.updated", "commit.pushed"],
+      },
+      targets: [
+        new targets.LambdaFunction(this.recordGithubEventDetailsHandler),
+      ],
+    });
+
+    new events.Rule(this, `PRDiffFetcherRule-${stageName}`, {
+      eventBus: props.eventBus,
+      enabled: true,
+      ruleName: `FetchDiffedChangesRule-${stageName}`,
+      description: "Fetches diffed changes for PRs or commits",
+      eventPattern: {
+        source: ["vortex.github"],
+        detailType: ["pr.created", "pr.updated", "commit.pushed"],
+      },
+      targets: [new targets.LambdaFunction(this.fetchDiffedChangesHandler)],
+    });
+
+    new events.Rule(this, `DiffAnalysisRule-${stageName}`, {
+      eventBus: props.eventBus,
+      enabled: true,
+      ruleName: `AnalyzeDiffRule-${stageName}`,
+      description: "Analyzes diffed changes for PRs",
+      eventPattern: {
+        source: ["vortex.github"],
+        detailType: ["diff.ready"],
+      },
+      targets: [new targets.LambdaFunction(this.lambdaAnalyzeDiff)],
+    });
   }
 }
 
