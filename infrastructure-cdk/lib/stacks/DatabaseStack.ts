71d6c565ad1521fc10dc7c2f14493ddaa5d0e8c5
Sandjio <emmasandjio@gmail.com>
2025-06-23 16:40:20 +0100

    feat: enhance DatabaseStack to support stage-specific table naming and removal policies

--- infrastructure-cdk/lib/stacks/DatabaseStack.ts
+++ infrastructure-cdk/lib/stacks/DatabaseStack.ts
@@ -7,20 +7,28 @@ import {
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+interface DatabaseStackProps extends StackProps {
+  stageName: string;
+}
 export class DatabaseStack extends Stack {
   public readonly table: dynamodb.Table;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: DatabaseStackProps) {
     super(scope, id, props);
 
-    this.table = new dynamodb.Table(this, "PullRequestsTable", {
-      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING }, // TODO: Use a more appropriate partition key
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: RemovalPolicy.RETAIN,
-    });
-
-    new CfnOutput(this, "TableName", {
-      value: this.table.tableName,
-    });
+    this.table = new dynamodb.Table(
+      this,
+      `PullRequestsTable-${props.stageName}`,
+      {
+        partitionKey: { name: "PK", type: dynamodb.AttributeType.STRING },
+        sortKey: { name: "SK", type: dynamodb.AttributeType.STRING },
+        tableName: `PullRequestsTable-${props.stageName}`,
+        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        removalPolicy:
+          props.stageName === "prod"
+            ? RemovalPolicy.RETAIN
+            : RemovalPolicy.DESTROY,
+      }
+    );
   }
 }
