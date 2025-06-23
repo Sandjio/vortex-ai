bfb0e0fa6532b26046e2561efe6c9a48a7dc46ab
Sandjio <emmasandjio@gmail.com>
2025-06-23 16:38:33 +0100

    feat: add EventBridgeStack and GitHubOidcRoleStack to infrastructure setup

--- infrastructure-cdk/bin/infrastructure-cdk.ts
+++ infrastructure-cdk/bin/infrastructure-cdk.ts
@@ -3,6 +3,8 @@ import * as cdk from "aws-cdk-lib";
 import { LambdaStack } from "../lib/stacks/LambdaStack";
 import { DatabaseStack } from "../lib/stacks/DatabaseStack";
 import { ApiStack } from "../lib/stacks/ApiStack";
+import { GitHubOidcRoleStack } from "../lib/stacks/GitHubOidcRoleStack";
+import { EventBridgeStack } from "../lib/stacks/EventBridgeStack";
 
 const app = new cdk.App();
 
@@ -10,24 +12,37 @@ let stageName = app.node.tryGetContext("stageName");
 let ssmStageName = app.node.tryGetContext("ssmStageName");
 
 if (!stageName) {
-  console.log("Defaulting to dev stage");
+  console.log("Defaulting to dev stage"); // throw new Error("❌ Missing context variable: 'stageName'. Pass with --context stageName=dev");
   stageName = "dev";
 }
 
 if (!ssmStageName) {
-  console.log(`Defaulting SSM stage name to "stageName":${stageName}`);
+  console.log(`Defaulting SSM stage name to "stageName":${stageName}`); // throw new Error("❌ Missing context variable: 'ssmStageName'. Pass with --context ssmStageName=dev");
   ssmStageName = stageName;
 }
 
 try {
-  const dbStack = new DatabaseStack(app, `DatabaseStack-${stageName}`, {});
+  const eventBridgeStack = new EventBridgeStack(
+    app,
+    `EventBridgeStack-${stageName}`,
+    {
+      stageName,
+    }
+  );
+
+  const dbStack = new DatabaseStack(app, `DatabaseStack-${stageName}`, {
+    stageName,
+  });
+
   const lambdaStack = new LambdaStack(app, `LambdaStack-${stageName}`, {
     table: dbStack.table,
+    eventBus: eventBridgeStack.eventBus,
   });
 
   new ApiStack(app, `ApiStack-${stageName}`, {
     handler: lambdaStack.webhookHandler,
   });
+  new GitHubOidcRoleStack(app, `GitHubOidcRoleStack-${stageName}`, {});
 } catch (error) {
   console.error("Error creating stacks:", error);
   process.exit(1);
